2e54c7593fa82c57c88a38d7c4010668e0477102
Mikhail Kiyatkin <33297177+NesQuick@users.noreply.github.com>
2020-04-20 11:01:14 +0300

    Create abstract.js

---
+++ HW3/abstract.js
@@ -0,0 +1,42 @@
+//landscape-in-minimalism generator
+let increment = 0.1;
+let timeOffset = 0.01;
+let stride = 20; 
+  
+function setup () {
+  createCanvas(windowWidth, windowHeight)
+  frameRate(5);
+}
+
+function draw() {
+  background(0);
+  
+  let offset = width/10;
+  
+  let yOffset = 0;
+  for (let x = offset; x < width - offset; x += stride) {
+    let xOffset = 0;
+    for (let y = offset; y < height - offset; y += stride) {
+      xOffset += increment;
+      push();
+      let randomGrey = noise(xOffset, yOffset, timeOffset) * 255
+      translate(x, y);
+      strokeWeight(8 * noise(xOffset, yOffset, timeOffset));
+      stroke(randomGrey);
+      noFill();
+      let diam = 20;
+      let r = noise(xOffset, yOffset, timeOffset);
+      let grad = 0;
+      
+      if (r > 0.5) {
+        grad = PI/3;
+      }
+      
+      triangle(diam*cos(PI/2 + grad)/2, diam*sin(PI/2 + grad)/2, diam*cos(7*PI/6 + grad)/2, diam*sin(7*PI/6 + grad)/2, diam*cos(11*PI/6 + grad)/2, diam*sin(11*PI/6 + grad)/2);
+      pop();
+    }
+    timeOffset += increment * 0.02;
+    yOffset += increment;
+    //noLoop(); //если нужна картинка
+  }
+}
